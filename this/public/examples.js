9af7d5cb73a0fb2eb11a3ab4f36e37344f479231
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2019-03-18 09:29:06 +0000

    Get it ready to talk through the examples.

--- this/public/examples.js
+++ this/public/examples.js
@@ -1,104 +1,157 @@
-class ClassyFoo {
-  constructor(name) {
-    this.name = name;
-  }
-
-  sayHi() {
-    console.log(`Enchanté. I'm ${this.name}`);
-  }
-}
-
-
-const FunFoo = function(name) {
-  this.name = name;
-
-  this.sayHi = function() {
-    console.log(`Hello I'm ${this.name} and I like mushrooms.`);
-  }
-}
-
-const json = {
-  name: "Jason",
-  sayHi: function () {
-    console.log(`Um... hi. I'm just ${this.name}, I guess.`);
-  }
-}
-
-const classyFoo = new ClassyFoo("Cici");
-classyFoo.sayHi();
-const funFoo = new FunFoo('Eric');
-funFoo.sayHi();
-json.sayHi();
-
-///////////////////////////////////////////////////////////////////////
-
-console.log("A bare this", this === window);
-
-///////////////////////////////////////////////////////////////////////
-
-function thisIsTheWindow() {
-  // "use strict";
-  console.log("This is the window", this === window);
-}
-
-thisIsTheWindow();
-
-///////////////////////////////////////////////////////////////////////
-
-const button = document.getElementById("eventful-button");
-
-function onButtonClick() {
-  console.log("This in a button click", this === window, this);
-}
-
-button.addEventListener("click", onButtonClick);
-
-/////////////////////////////////////////////////////////////////////////
-
-function myVeryImpressionableFunction() {
-  console.log("Today I am bound eternally to...", this);
-}
-
-myVeryImpressionableFunction();
-
 
 (function() {
-  // "use strict";
-  myVeryImpressionableFunction.call(null);
+  console.log("A bare this", this === window);
+});
+
+///////////////////////////////////////////////////////////////////////
+
+(function() {
+  function whatDoesThisBelongTo() {
+    console.log("This is the window", this === window);
+  }
+
+  // whatDoesThisBelongTo();
+});
+
+///////////////////////////////////////////////////////////////////////
+
+(function() {
+  class ClassyFoo {
+    constructor(name) {
+      this.name = name;
+    }
+
+    sayHi() {
+      console.log(`Enchanté. I'm ${this.name}`);
+    }
+  }
+
+  const FunFoo = function(name) {
+    this.name = name;
+
+    this.sayHi = function() {
+      console.log(`Hello I'm ${this.name} and I like mushrooms.`);
+    }
+  }
+
+  const json = {
+    name: "Jason",
+    sayHi: function () {
+      console.log(`Um... hi. I'm just ${this.name}, I guess.`);
+    }
+  }
+
+  const classyFoo = new ClassyFoo("Cici");
+  const funFoo = new FunFoo('Eric');
+
+  // classyFoo.sayHi();
+  // funFoo.sayHi();
+  // json.sayHi();
 })();
 
-myVeryImpressionableFunction.apply("Something completely different.");
-
-const boundImpressionableFunction = myVeryImpressionableFunction.bind({ id: "This is an object now..."});
-boundImpressionableFunction();
-myVeryImpressionableFunction();
-
 /////////////////////////////////////////////////////////////////////////
 
-const arrowThis = (message) => {
-  // "use strict";
-  console.log(message, this);
-};
+(function(){;
+  const someObject = {
+    i: 10,
+    arrow: () => {
+      console.log("From the arrow:", this.i);
+    },
+    func: function () {
+      console.log("From the normal function:", this.i);
+    }
+  };
 
-arrowThis("If an arrow points at this what does it become?");
+  // someObject.func();
+  // someObject.arrow();
+})();
 
-/////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 
-arrowThis.bind("Is it me?")("Is it this this?");
-arrowThis.call("Let it be me!", "If not that this What about this this?");
-arrowThis.apply("Surely it'll be me?", ["If not that this What about this this?"]);
+(function() {
+  const button = document.getElementById("eventful-button");
 
-/////////////////////////////////////////////////////////////////////////
-
-const someObject = {
-  i: 10,
-  arrow: () => {
-    console.log("From the arrow:", this.i);
-  },
-  func: function () {
-    console.log("From the normal function:", this.i);
+  function onButtonClick() {
+    console.log("This in a button click", this === window);
   }
-};
 
-someObject.func();
-someObject.arrow();
+  button.addEventListener("click", onButtonClick);
+})();
+
+///////////////////////////////////////////////////////////////////////
+
+(function() {
+  "use strict";
+
+  function whatDoesThisBelongTo() {
+    console.log("This is the window", this === window);
+  }
+
+  // whatDoesThisBelongTo();
+})();
+
+/////////////////////////////////////////////////////////////////////////
+
+(function() {
+  function myVeryImpressionableFunction() {
+    console.log("Today I am bound eternally to...", this);
+  }
+
+  // myVeryImpressionableFunction();
+
+  // myVeryImpressionableFunction.call(null);
+
+  (function() {
+    "use strict";
+    // myVeryImpressionableFunction.call(null);
+  })();
+
+  // myVeryImpressionableFunction.apply("Something completely different.");
+
+  const boundImpressionableFunction = myVeryImpressionableFunction.bind({ id: "This is an object now..."});
+  // boundImpressionableFunction();
+  // myVeryImpressionableFunction();
+})();
+
+/////////////////////////////////////////////////////////////////////////
+
+(function() {
+  const arrowThis = (message) => {
+    // "use strict";
+    console.log(message, this);
+  };
+
+  // arrowThis("If an arrow points at this what does it become?");
+
+  // arrowThis.bind("Is it me?")("Is it this this?");
+  // arrowThis.call("Let it be me!", "If not that this What about this this?");
+  // arrowThis.apply("Surely it'll be me?", ["If not that this What about this this?"]);
+})();
+
+//////////////////////////////////////////////////////////////////////////
+
+(function() {
+  function FunctionHireShop() {
+    this.otherFunction = function () {
+      console.log("Here I am!")
+    }
+
+    this.someUtilityFunction = function () {
+      console.log("I need my buddy to work...", this.otherFunction())
+    }
+  }
+
+  function DiyFunctionUser(someUtilityFunction) {
+    this.someUtilityFunction = someUtilityFunction;
+
+    this.doSomeDiy = function () {
+      console.log("I just reach for that function i got from the shop...", this.someUtilityFunction())
+    }
+  }
+
+  const hireShop = new FunctionHireShop();
+  const eagerDiyer = new DiyFunctionUser(hireShop.someUtilityFunction);
+
+  eagerDiyer.doSomeDiy();
+});
+
