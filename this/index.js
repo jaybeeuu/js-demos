4fe21e0737a71b24124fcbca3f349916e93c5cbd
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2019-03-25 21:49:04 +0000

    Add Markdown process to this

--- this/index.js
+++ this/index.js
@@ -1,9 +1,32 @@
 const express = require('express')
+const fs = require('fs');
+const showdown = require('showdown');
+
 const app = express()
 const port = 3000
 
+const textPromise = new Promise((resolve, reject) => {
+  fs.readFile('README.md', { encoding: 'utf8' }, (err, content) => {
+    if(err) {
+      reject(err);
+      return;
+    }
+
+    const converter = new showdown.Converter()
+    const htmlContent = converter.makeHtml(content);
+    resolve(htmlContent);
+  });
+});
+
 app.get('/', function(req, res){
   res.sendFile('index.html', { root: __dirname + "/public" } );
+});
+
+app.get('/text', function(req, res, next) {
+  textPromise.then((htmlContent) => {
+    res.type('html');
+    res.send(htmlContent);
+  }).catch(next);
 });
 
 app.use(express.static('public'));
